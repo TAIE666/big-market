18a6fb0d4ff07621c0f5f19811d612c77cf1f377
Coolcoudou <1114008281@qq.com>
2025-05-26 14:19:46 +0800

    脚手架

---
+++ big-market-infrastructure/src/main/java/cn/coudou/infrastructure/persistent/dao/IStrategyDao.java
@@ -0,0 +1,11 @@
+package cn.coudou.infrastructure.persistent.dao;
+
+import cn.coudou.infrastructure.persistent.po.Strategy;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface IStrategyDao {
+    List<Strategy> queryStrategyList();
+}
