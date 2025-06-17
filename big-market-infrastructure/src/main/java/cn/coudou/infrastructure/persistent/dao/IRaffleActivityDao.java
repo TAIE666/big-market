6cd598def0b211d9d13357ef55bfa14463c97106
Coolcoudou <1114008281@qq.com>
2025-06-17 14:23:54 +0800

    引入分库分表路由组件

---
+++ big-market-infrastructure/src/main/java/cn/coudou/infrastructure/persistent/dao/IRaffleActivityDao.java
@@ -0,0 +1,14 @@
+package cn.coudou.infrastructure.persistent.dao;
+
+import cn.coudou.infrastructure.persistent.po.RaffleActivity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 抽奖活动表Dao
+ */
+@Mapper
+public interface IRaffleActivityDao {
+
+    RaffleActivity queryRaffleActivityByActivityId(Long activityId);
+
+}
---
+++ big-market-infrastructure/src/main/java/cn/coudou/infrastructure/persistent/dao/IRaffleActivityOrderDao.java
@@ -0,0 +1,23 @@
+package cn.coudou.infrastructure.persistent.dao;
+
+import cn.bugstack.middleware.db.router.annotation.DBRouter;
+import cn.bugstack.middleware.db.router.annotation.DBRouterStrategy;
+import cn.coudou.infrastructure.persistent.po.RaffleActivityOrder;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * 抽奖活动单Dao
+ */
+@Mapper
+@DBRouterStrategy(splitTable = true)
+public interface IRaffleActivityOrderDao {
+
+    @DBRouter(key = "userId")
+    void insert(RaffleActivityOrder raffleActivityOrder);
+
+    @DBRouter
+    List<RaffleActivityOrder> queryRaffleActivityOrderByUserId(String userId);
+
+}
